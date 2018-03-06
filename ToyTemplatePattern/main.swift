62364b9b8ee2757fd6d3529813a6af8430e923b1
Faiz Mokhtar <mfmokhtar@gmail.com>
2018-03-06 19:37:57 +0800

    Initial Commit

---
+++ ToyTemplatePattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ToyTemplatePattern
+//
+//  Created by Faiz Mokhtar on 06/03/2018.
+//  Copyright © 2018 Faiz Mokhtar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
